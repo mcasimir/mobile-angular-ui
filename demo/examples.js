75b4896f416c2751b0e0eda1802e9c7e4d236ce9
mcasimir <maurizio.cas@gmail.com>
2013-12-27 10:19:15 +0100

    push

--- demo/examples.js
+++ demo/examples.js
@@ -1,13 +1,13 @@
-var app = angular.module('AngularUiMobileExamples', ["mobile-angular-ui", "ngRoute"]);
+var app = angular.module('MobileAngularUiExamples', ["mobile-angular-ui", "ngRoute"]);
 
 app.config(function($routeProvider, $locationProvider) {
-  $routeProvider.when('/',          {templateUrl: "./sink.html"});
-  $routeProvider.when('/scroll',    {templateUrl: "./scroll.html"}); 
-  $routeProvider.when('/toggle',    {templateUrl: "./toggle.html"}); 
-  $routeProvider.when('/tabs',      {templateUrl: "./tabs.html"}); 
-  $routeProvider.when('/accordion', {templateUrl: "./accordion.html"}); 
-  $routeProvider.when('/overlay',   {templateUrl: "./overlay.html"}); 
-  $routeProvider.when('/forms',     {templateUrl: "./forms.html"});
+  $routeProvider.when('/',          {templateUrl: "home.html"});
+  $routeProvider.when('/scroll',    {templateUrl: "scroll.html"}); 
+  $routeProvider.when('/toggle',    {templateUrl: "toggle.html"}); 
+  $routeProvider.when('/tabs',      {templateUrl: "tabs.html"}); 
+  $routeProvider.when('/accordion', {templateUrl: "accordion.html"}); 
+  $routeProvider.when('/overlay',   {templateUrl: "overlay.html"}); 
+  $routeProvider.when('/forms',     {templateUrl: "forms.html"});
 });
 
 app.filter('range', function() {
