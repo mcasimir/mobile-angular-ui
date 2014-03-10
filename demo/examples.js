001324092728d202dc4d9654cba07d5120c73f4e
mcasimir <maurizio.cas@gmail.com>
2014-03-10 18:09:27 +0100

    push

--- demo/examples.js
+++ demo/examples.js
@@ -13,6 +13,7 @@ app.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/accordion', {templateUrl: "accordion.html"}); 
   $routeProvider.when('/overlay',   {templateUrl: "overlay.html"}); 
   $routeProvider.when('/forms',     {templateUrl: "forms.html"});
+  $routeProvider.when('/carousel',  {templateUrl: "carousel.html"});
 });
 
 app.service('analytics', [
@@ -22,6 +23,58 @@ app.service('analytics', [
     }
   }
 ]);
+
+app.directive( "carouselExampleItem", function($rootScope, $swipe){
+  return function(scope, element, attrs){
+      var startX = null;
+      var startY = null;
+      var endAction = "cancel";
+      var carouselId = element.parent().parent().attr("id");
+
+      var translateAndRotate = function(x, y, z, deg){
+        element[0].style["-webkit-transform"] = "translate3d("+x+"px,"+ y +"px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-moz-transform"] = "translate3d("+x+"px," + y +"px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-ms-transform"] = "translate3d("+x+"px," + y + "px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-o-transform"] = "translate3d("+x+"px," + y  + "px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["transform"] = "translate3d("+x+"px," + y + "px," + z + "px) rotate("+ deg +"deg)";
+      }
+
+      $swipe.bind(element, {
+        start: function(coords) {
+          startX = coords.x;
+          startY = coords.y;
+        },
+
+        cancel: function(e) {
+          translateAndRotate(0, 0, 0, 0);
+          e.stopPropagation();
+        },
+
+        end: function(coords, e) {
+          if (endAction == "prev") {
+            $rootScope.carouselPrev(carouselId);
+          } else if (endAction == "next") {
+            $rootScope.carouselNext(carouselId);
+          }
+          translateAndRotate(0, 0, 0, 0);
+          e.stopPropagation();
+        },
+
+        move: function(coords) {
+          if( startX != null) {
+            var deltaX = coords.x - startX;
+            var deltaXRatio = deltaX / element[0].clientWidth;
+            if (deltaXRatio > 0.3) {
+              endAction = "next";
+            } else if (deltaXRatio < -0.3){
+              endAction = "prev";
+            }
+            translateAndRotate(deltaXRatio * 200, 0, 0, deltaXRatio * 15);
+          }
+        }
+      });
+    }
+});
 
 app.controller('MainController', function($rootScope, $scope, analytics){
 
