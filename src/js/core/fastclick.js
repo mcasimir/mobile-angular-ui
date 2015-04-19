f2ffd654a23f137a9a6857c65cb968dde6683dcb
Remco <remco@fritzy.nl>
2015-04-20 00:19:38 +0200

    Fastclick fix: mcasimir/mobile-angular-ui#230
    Investigated this, and I think it has never worked correctly. The
    recommended way of attaching Fastclick isn't working with AngularJS.
    
    It is being attached to the DOMContentloaded event, which in a SPA fires
    only once and after that angular is starting:
    > Angular initializes automatically upon DOMContentLoaded event or when
    the angular.js script is evaluated if at that time document.readyState
    is set to 'complete'. At this point Angular looks for the ng-app
    directive which designates your application root.

--- src/js/core/fastclick.js
+++ src/js/core/fastclick.js
@@ -2,15 +2,12 @@
   'use strict';
   var module = angular.module('mobile-angular-ui.core.fastclick', []);
 
-  module.run(function() {
-    if ('addEventListener' in document) {
-      document.addEventListener('DOMContentLoaded', function() {
-          FastClick.attach(document.body);
-      }, false);
-    }
-  });
+  module.run(['$window', function($window) {
+	FastClick.attach($window.document.body);  
+  }]);
 
   angular.forEach(['select', 'input', 'textarea'], function(directiveName){
+  
     module.directive(directiveName, function(){
       return {
         restrict: 'E',
