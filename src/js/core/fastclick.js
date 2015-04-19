637c1298a5dae53067d2c8429f66b7d66e78b041
Remco <remco@fritzy.nl>
2015-04-20 01:51:44 +0200

    Fixes: #187  Overthrow / fastclick error
    Fix for overthrow/fastclick error. Capturing ontouchEnd event and added
    missing property.

--- src/js/core/fastclick.js
+++ src/js/core/fastclick.js
@@ -1,9 +1,30 @@
+var orgHandler = null;
+
 (function () {
   'use strict';
   var module = angular.module('mobile-angular-ui.core.fastclick', []);
 
   module.run(['$window', function($window) {
-	FastClick.attach($window.document.body);  
+  
+	//Temporarly bugfix in overthrow/fastclick:
+	orgHandler = FastClick.prototype.onTouchEnd;
+	
+	// Some old versions of Android don't have Function.prototype.bind
+	function bind(method, context) {
+		return function() { return method.apply(context, arguments); };
+	}
+		
+	FastClick.prototype.onTouchEnd = function(event) {
+			
+		if (!event.changedTouches)
+			event.changedTouches = [{}];
+
+		orgHandler = bind(orgHandler, this);
+		orgHandler(event);
+	}
+	
+	FastClick.attach($window.document.body);  	
+	
   }]);
 
   angular.forEach(['select', 'input', 'textarea'], function(directiveName){
