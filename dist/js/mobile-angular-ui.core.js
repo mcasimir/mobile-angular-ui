d44ef94ea19f75a94e03aa884978214f77f85b10
mcasimir <maurizio.cas@gmail.com>
2015-08-04 19:34:37 +0200

    build+bump

--- dist/js/mobile-angular-ui.core.js
+++ dist/js/mobile-angular-ui.core.js
@@ -915,96 +915,96 @@ a.active {
 }());
 
 
-/** 
+/**
  * @module mobile-angular-ui.core.capture
  * @description
- * 
+ *
  * The `capture` module exposes directives to let you extract markup which can be used in other parts of a template using `uiContentFor` and `uiYieldTo` directives.
- * 
+ *
  * It provides a way to move or clone a block of markup to other parts of the document.
- * 
+ *
  * This method is particularly useful to setup parts of the layout within an angular view. Since blocks of html are transplanted within their original `$scope` is easy to create layout interactions depending on the context. Some tipical task you can accomplish with these directives are: _setup the navbar title depending on the view_ or _place a submit button for a form inside a navbar_.
- * 
+ *
  * ## Usage
- * 
+ *
  * Declare it as a dependency to your app unless you have already included some of its super-modules.
- * 
+ *
  * ```
  * angular.module('myApp', ['mobile-angular-ui']);
  * ```
- * 
+ *
  * Or
- * 
+ *
  * ```
  * angular.module('myApp', ['mobile-angular-ui']);
  * ```
- * 
+ *
  * Or
- * 
+ *
  * ```
  * angular.module('myApp', ['mobile-angular-ui.core.capture']);
  * ```
- * 
+ *
  * Use `ui-yield-to` as a placeholder.
- * 
+ *
  * ``` html
  * <!-- index.html -->
- * 
+ *
  * <div class="navbar">
  *   <div ui-yield-to="title" class="navbar-brand">
  *     <span>Default Title</span>
  *   </div>
  * </div>
- * 
+ *
  * <div class="app-body">
  *   <ng-view class="app-content"></ng-view>
  * </div>
  * ```
- * 
+ *
  * Use `ui-content-for` inside any view to populate the `ui-yield-to` content.
- * 
+ *
  * ``` html
  * <!-- myView.html -->
- * 
+ *
  * <div ui-content-for="title">
  *   <span>My View Title</span>
  * </div>
  * ```
- * 
- * Since the original scope is preserved you can use directives inside `ui-content-for` blocks to interact with the current scope. In the following example we will add a navbar button to submit a form inside a nested view.  
- * 
- * 
+ *
+ * Since the original scope is preserved you can use directives inside `ui-content-for` blocks to interact with the current scope. In the following example we will add a navbar button to submit a form inside a nested view.
+ *
+ *
  * ``` html
  * <!-- index.html -->
- * 
+ *
  * <div class="navbar">
  *   <div ui-yield-to="navbarAction">
  *   </div>
  * </div>
- * 
+ *
  * <div class="app-body">
  *   <ng-view class="app-content"></ng-view>
  * </div>
  * ```
- * 
+ *
  * ``` html
  * <!-- newCustomer.html -->
- * 
+ *
  * <form ng-controller="newCustomerController">
- * 
+ *
  *   <div class="inputs">
- *     <input type="text" ng-model="customer.name" />  
+ *     <input type="text" ng-model="customer.name" />
  *   </div>
- * 
+ *
  *   <div ui-content-for="navbarAction">
  *     <button ng-click="createCustomer()">
  *       Save
  *     </button>
  *   </div>
- * 
+ *
  * </form>
  * ```
- * 
+ *
  * ``` javascript
  * app.controller('newCustomerController', function($scope, Store){
  *   $scope.customer = {};
@@ -1014,9 +1014,9 @@ a.active {
  *   }
  * });
  * ```
- * 
+ *
  * If you wish you can also duplicate markup instead of move it. Just add `duplicate` parameter to `uiContentFor` directive to specify this behaviour.
- * 
+ *
  * ``` html
  * <div ui-content-for="navbarAction" duplicate>
  *   <button ng-click="createCustomer()">
@@ -1031,17 +1031,17 @@ a.active {
    angular.module('mobile-angular-ui.core.capture', [])
 
    .run([
-     'Capture', 
-     '$rootScope', 
+     'Capture',
+     '$rootScope',
      function(Capture, $rootScope) {
-       $rootScope.$on('$routeChangeStart', function() {
+       $rootScope.$on('$routeChangeSuccess', function() {
          Capture.resetAll();
        });
      }
    ])
 
    .factory('Capture', [
-     '$compile', 
+     '$compile',
      function($compile) {
        var yielders = {};
 
@@ -1049,11 +1049,11 @@ a.active {
          resetAll: function() {
            for (var name in yielders) {
             if (yielders.hasOwnProperty(name)) {
-              this.resetYielder(name); 
+              this.resetYielder(name);
             }
            }
          },
-         
+
          resetYielder: function(name) {
            var b = yielders[name];
            this.setContentFor(name, b.defaultContent, b.defaultScope);
@@ -1075,7 +1075,7 @@ a.active {
          removeYielder: function(name) {
            delete yielders[name];
          },
-         
+
          setContentFor: function(name, content, scope) {
            var b = yielders[name];
            if (!b) {
@@ -1093,20 +1093,20 @@ a.active {
    * @directive uiContentFor
    * @restrict A
    * @description
-   * 
-   * `ui-content-for` makes inner contents to replace the corresponding 
+   *
+   * `ui-content-for` makes inner contents to replace the corresponding
    * `ui-yield-to` placeholder contents.
    *
    * `uiContentFor` is intended to be used inside a view in order to populate outer placeholders.
    * Any content you send to placeholders via `ui-content-for` is
    * reverted to placeholder defaults after view changes (ie. on `$routeChangeStart`).
-   * 
+   *
    * @param {string} uiContentFor The id of the placeholder to be replaced
    * @param {boolean} uiDuplicate If present duplicates the content instead of moving it (default to `false`)
    *
    */
    .directive('uiContentFor', [
-     'Capture', 
+     'Capture',
      function(Capture) {
        return {
          compile: function(tElem, tAttrs) {
@@ -1132,7 +1132,7 @@ a.active {
     * `ui-yield-to` defines a placeholder which contents will be further replaced by `ui-content-for` directive.
     *
     * Inner html is considered to be a default. Default is restored any time `$routeChangeStart` happens.
-    * 
+    *
     * @param {string} uiYieldTo The unique id of this placeholder.
     *
     */
@@ -1141,11 +1141,11 @@ a.active {
        return {
          link: function(scope, element, attr) {
            Capture.putYielder(attr.uiYieldTo, element, scope, element.html());
-           
+
            element.on('$destroy', function(){
              Capture.removeYielder(attr.uiYieldTo);
            });
-           
+
            scope.$on('$destroy', function(){
              Capture.removeYielder(attr.uiYieldTo);
            });
@@ -1155,6 +1155,7 @@ a.active {
    ]);
 
 }());
+
 (function () {
   'use strict';
   var module = angular.module('mobile-angular-ui.core.fastclick', []);
