0a5ea0d2f1ff8e2fb3d96ec99c6dd92755b1c6b3
mcasimir <maurizio.cas@gmail.com>
2015-08-06 14:21:13 +0200

    test travis with sauceconnect

--- test/ci.conf.js
+++ test/ci.conf.js
@@ -5,6 +5,8 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
   capabilities: {
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
     'browserName': 'chrome'
   },
   specs: [
