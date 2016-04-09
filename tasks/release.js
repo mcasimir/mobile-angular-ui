335c2ddb6c4b962b20541feb1495f50b80b29588
mcasimir <maurizio.cas@gmail.com>
2016-04-09 23:37:56 +0200

    chore: added release task

--- tasks/release.js
+++ tasks/release.js
@@ -2,4 +2,12 @@
 
 module.exports = function(gulp) {
   require('gulp-release-tasks')(gulp);
+
+  var assertClean = require('git-assert-clean');
+
+  gulp.task('release:check-status', function() {
+    return assertClean();
+  });
+
+  gulp.task('release', require('gulp-sequence')('release:check-status', 'build', 'test:ci', 'tag', 'changelog:commit'));
 };
