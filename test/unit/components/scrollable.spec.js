7e2de013cc59293313609be087ccc2ecbbd733c4
mcasimir <maurizio.cas@gmail.com>
2016-04-10 12:01:59 +0200

    test: initial coverage of scrollable component

--- test/unit/components/scrollable.spec.js
+++ test/unit/components/scrollable.spec.js
@@ -4,16 +4,36 @@ describe('components', function() {
   describe('scrollable', function() {
     var scope;
     var compile;
-
     beforeEach(function() {
-      module('mobile-angular-ui.components');
+      module('mobile-angular-ui.components.scrollable');
       inject(function($rootScope, $compile) {
         scope = $rootScope.$new();
         compile = $compile;
       });
     });
 
-    xit('should be able to scroll to an element', function() {});
-    xit('should center an element when focused and window resizes (aka soft keyboard workaround)', function() {});
+    describe('scrollableContent', function() {
+      it('adds overthrow class if overthrow support is not native', function() {
+        var bkpOverthrowSupport = window.overthrow.support;
+        window.overthrow.support = 'polyfilled';
+        var elem = angular.element('<div class="scrollable-content" />');
+        compile(elem)(scope);
+        scope.$digest();
+        expect(elem.attr('class')).toContain('overthrow');
+        window.overthrow.support = bkpOverthrowSupport;
+      });
+
+      it('does not add overthrow class if overthrow support is native', function() {
+        var bkpOverthrowSupport = window.overthrow.support;
+        window.overthrow.support = 'native';
+        var elem = angular.element('<div class="scrollable-content" />');
+        compile(elem)(scope);
+        scope.$digest();
+        expect(elem.attr('class')).not.toContain('overthrow');
+        window.overthrow.support = bkpOverthrowSupport;
+      });
+
+      xit('should be able to scroll to an element', function() {});
+    });
   });
 });
